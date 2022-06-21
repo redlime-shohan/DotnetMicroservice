e3b1291b72e4e0cb08b589286e839eac01e20300
Md.Rafiul Islam Shohan <redlime.shohan@gmail.com>
2022-06-21 09:13:13 +0600

    complete application layer

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/IUnhandledExceptionBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Behaviours
+{
+    public interface IUnhandledExceptionBehaviour<TRequest, TResponse>
+    {
+        Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next);
+    }
+}
