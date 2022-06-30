26b9468bf49c86c5e029d1c3770fa2915b008c0e
Md.Rafiul Islam Shohan <redlime.shohan@gmail.com>
2022-06-30 14:38:52 +0600

    configure program.cs

--- src/OcelotApiGw/Program.cs
+++ src/OcelotApiGw/Program.cs
@@ -1,5 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
+{
+    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
+
+
 var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 
