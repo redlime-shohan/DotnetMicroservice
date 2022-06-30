cf1363b02487ae1bb1c5681a90ff6849ea7ad8b6
Md.Rafiul Islam Shohan <redlime.shohan@gmail.com>
2022-06-30 16:13:13 +0600

    add and configure ocelot

--- src/OcelotApiGw/Program.cs
+++ src/OcelotApiGw/Program.cs
@@ -1,4 +1,10 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot();
+
 builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
 {
     loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
@@ -6,10 +12,17 @@ builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
     loggingBuilder.AddDebug();
 });
 
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json",true,true);
+});
 
 var app = builder.Build();
 
 
+
 app.MapGet("/", () => "Hello World!");
+
+app.UseOcelot();
 
 app.Run();
